3733cca5f93c19096b4a0a46a1f574ae5be17a4e
Mert <mertkavilci@gmail.com>
2025-07-27 19:26:13 +0300

    Added Radar Detection

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static class App {
         public static void main(String[] args) {
@@ -12,15 +14,18 @@ public class Main {
         public Unit createUnit(String name) {
             Unit u = new Unit();
             u.name = name;
+
             u.pos = new Position();
             u.pos.x = ThreadLocalRandom.current().nextInt(10);
             u.pos.y = ThreadLocalRandom.current().nextInt(10);
+
             u.speed = new Speed();
             u.speed.x = ThreadLocalRandom.current().nextInt(3);
             u.speed.y = ThreadLocalRandom.current().nextInt(3);
 
-            Radar r = new Radar();
+            Radar r = new Radar(u);
             u.components.add(r);
+
             return u;
         }
         public void run() {
@@ -28,6 +33,7 @@ public class Main {
             boolean isWorking = true;
             int time = 0;
             int updateInterval = 1000;
+
             World w = new World();
             w.units.add(createUnit("Mert"));
             w.units.add(createUnit("Emir"));
@@ -56,7 +62,7 @@ public class Main {
     }
     public static class World  {
         WorldMap map;
-        ArrayList<Unit> units = new ArrayList<>();
+        static ArrayList<Unit> units = new ArrayList<>();
 
         void update(int deltaTime) {
             System.out.println("World::update");
@@ -85,29 +91,61 @@ public class Main {
         ArrayList<Component> components = new ArrayList<>();
 
         void update(int deltaTime) {
-
             pos.x += speed.x;
             pos.y += speed.y;
             System.out.println("Unit::update -" + name + " - " + "time:" + deltaTime + " position: " + pos.x + " " + pos.y + " speed: " + speed.x + " " + speed.y);
             for (int i = 0; i < components.size(); i++) {
                 components.get(i).update(deltaTime);
+                components.get(i).setPosition(pos);
             }
 
         }
     }
     public static abstract class Component {
+        Unit parentUnit;
+
+        public Component(Unit parent){
+            parentUnit = parent;
+        }
+
         public abstract void update(int deltaTime);
+        void setPosition(Position pos){
+            parentUnit.pos = pos;
+        }
     }
     public static class Radar extends Component {
-        int range = 500;
+        int range = 10;
+
+        public Radar(Unit parent) {
+            super(parent);
+        }
+
 
         void detect(Position selfPosition, List<Unit> units) {
             // calc distance for every unit
             // check range
+
+            boolean hasVisual = false;
+            for (int i = 0; i < units.size(); i++) {
+                if(sqrt((units.get(i).pos.x - selfPosition.x)*(units.get(i).pos.x - selfPosition.x) + (units.get(i).pos.y - selfPosition.y)) <= range && (units.get(i).pos.x != selfPosition.x || units.get(i).pos.y != selfPosition.y)){
+                    System.out.println("ComponentRadar::update Unit " + parentUnit.name.toUpperCase() + " has detected unit " + units.get(i).name.toUpperCase());
+                    hasVisual = true;
+
+                    // Position check for distance calculation accuracy
+                    // System.out.println("Position of unit " + parentUnit.name.toUpperCase() + " : " + parentUnit.pos.x + " " + parentUnit.pos.y);
+                    // System.out.println("Position of unit " + units.get(i).name.toUpperCase() + " : " + units.get(i).pos.x + " " + units.get(i).pos.y);
+
+                }
+                //System.out.println("ComponentRadar::update Unit " + parentUnit.name.toUpperCase() + " has no detection ");
+            }
+            if(!hasVisual)
+                System.out.println("ComponentRadar::update Unit " + parentUnit.name.toUpperCase() + " has no detection ");
+            System.out.println("");
         }
 
         @Override
         public void update(int deltaTime) {
+            detect(parentUnit.pos, World.units);
             //System.out.println("ComponentRadar::update");
         }
     }
