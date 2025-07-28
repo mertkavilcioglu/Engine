e101f13efb24991ba62ead6355643d2a7c502f98
SEDA KOCAOĞLU <200404326+sedakcgl@users.noreply.github.com>
2025-07-28 09:18:19 +0300

    Changed the format of system.out in Unit class

--- src/Unit.java
+++ src/Unit.java
@@ -9,7 +9,7 @@ public class Unit {
     void update(int deltaTime) {
         pos.x += speed.x;
         pos.y += speed.y;
-        System.out.println("Unit::update -" + name + " - " + "time:" + deltaTime + " position: " + pos.x + " " + pos.y + " speed: " + speed.x + " " + speed.y);
+        System.out.format("Unit::update - %s - time: %d position: %d %d speed: %d %d\n", name, deltaTime, pos.x, pos.y, speed.x, speed.y); 
         for (int i = 0; i < components.size(); i++) {
             components.get(i).update(deltaTime);
             components.get(i).setPosition(pos);
