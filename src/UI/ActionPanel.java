bc44ce0321d885487a457c0844e76f1fa783fb3b
SEDA KOCAOĞLU <200404326+sedakcgl@users.noreply.github.com>
2025-08-05 15:20:23 +0300

    Update ActionPanel for currentpanel

--- src/UI/ActionPanel.java
+++ src/UI/ActionPanel.java
@@ -5,6 +5,7 @@ import Sim.Entity;
 import Vec.Vec2int;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class ActionPanel extends VCSpanel{
@@ -24,7 +25,7 @@ public class ActionPanel extends VCSpanel{
         this.setLayout(new BorderLayout());
 
         label = new JLabel("Selected Unit:", SwingConstants.CENTER);
-        JPanel panel = new JPanel(new GridLayout(1,3));
+        JPanel panel = new JPanel(new GridLayout(1,3,0,0));
 
         orderp = new JPanel(new GridLayout(5,1));
         attack = new JButton("Attack");
@@ -51,9 +52,12 @@ public class ActionPanel extends VCSpanel{
         movep.add(meditor);
         movep.add(mbutton);
 
-        currentp = new JPanel();
-        currentp.add(new JLabel("Current Order"));
-        currentp.add(new JTextField());
+        currentp = new JPanel(new FlowLayout(FlowLayout.CENTER,0,0));
+        JTextField text = new JTextField();
+        text.setBorder(new TitledBorder("Current Order"));
+        text.setEditable(false);
+        text.setPreferredSize(new Dimension(120,220));
+        currentp.add(new JScrollPane(text));
 
         JPanel showp = new JPanel(new CardLayout());
         JPanel empty = new JPanel();
@@ -61,24 +65,24 @@ public class ActionPanel extends VCSpanel{
         showp.add(targetp, "target");
         showp.add(movep, "move");
 
-        orderp.setSize(100,200);
+        orderp.setPreferredSize(new Dimension(120,220));
         orderp.setBorder(BorderFactory.createDashedBorder(Color.black));
-        showp.setSize(100,200);
+        showp.setPreferredSize(new Dimension(120,220));
         showp.setBorder(BorderFactory.createDashedBorder(Color.black));
-        currentp.setSize(100,200);
-        currentp.setBorder(BorderFactory.createDashedBorder(Color.black));
+        currentp.setPreferredSize(new Dimension(120,220));
+        //currentp.setBorder(BorderFactory.createDashedBorder(Color.black));
 
         CardLayout cardLayout = (CardLayout) showp.getLayout();
         attack.addActionListener(e -> cardLayout.show(showp, "target"));
         move.addActionListener(e -> cardLayout.show(showp, "move"));
 
         //butonların ActionListener ları attactan move a ya da move dan attack a geçince ekrana yazdırıyor ona bakmak lazım....
-        first.addActionListener(e -> currentp.add(new JLabel("First target selected.")));
-        second.addActionListener(e -> currentp.add(new JLabel("Second target selected.")));
-        third.addActionListener(e -> currentp.add(new JLabel("Third target selected.")));
-        mbutton.addActionListener(e -> currentp.add(new JLabel("Moving to " + meditor.readData())));
+        first.addActionListener(e -> text.setText("First target"));
+        second.addActionListener(e -> text.setText("Second target"));
+        third.addActionListener(e -> text.setText("Third target"));
+        mbutton.addActionListener(e -> text.setText("Moving to " + meditor.readData()));
 
-        
+
         panel.add(orderp);
         panel.add(showp);
         panel.add(currentp);
