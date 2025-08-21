376b7c2e8c1b054a6eee88d4b8444fca1c2ad662
SEDA KOCAOĞLU <200404326+sedakcgl@users.noreply.github.com>
2025-08-21 16:32:22 +0300

    Fixed the Move class acc. new changes.

--- src/Sim/Orders/Move.java
+++ src/Sim/Orders/Move.java
@@ -12,18 +12,18 @@ public class Move extends Order{
         super(app, e);
     }
 
-    public void moveTo(String unitName, Vec2int coordinates){
-        Entity unit = app.follow.findEntity(unitName);
-        double dist = unit.getPos().distance(coordinates);
+    public void moveTo(Vec2int coordinates){
+        double dist = source.getPos().distance(coordinates);
         if(dist <= 2.0){
-            app.log(unitName + " reached the target. \n");
-            unit.setSpeed(new Vec2int(0,0));
+            app.log(source.getName() + " reached the target. \n");
+            source.setSpeed(new Vec2int(0,0));
+            removeOrder();
             app.actionPanel.isMoving = false;
             return;
         }
         else{
-            Vec2int newSpeed = unit.getPos().vectorDiff(coordinates).normalize(4);
-            unit.setSpeed(newSpeed);
+            Vec2int newSpeed = source.getPos().vectorDiff(coordinates).normalize(4);
+            source.setSpeed(newSpeed);
         }
     }
 
