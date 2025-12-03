f268352557e366272fa1dd791bb642c00b14b1b4
Mert <122644315+mertkavilcioglu@users.noreply.github.com>
2025-12-03 11:27:16 +0300

    Updated Log Filters

--- src/UI/LogPanel.java
+++ src/UI/LogPanel.java
@@ -6,8 +6,6 @@ import Sim.TDL.Message;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
-import javax.swing.border.MatteBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
@@ -23,10 +21,13 @@ import java.util.HashMap;
 
 public class LogPanel extends VCSPanel {
     private JTextPane logArea;
-    private final DefaultListModel<Message> messageModel;
+    private final DefaultListModel<Message> messageModelAll;
     private final JList<Message> messageList;
     private Border defaultBorder;
     private HashMap<Message.MessageClass, JButton> filterButtons = new HashMap<>();
+    private HashMap<Message.MessageClass, JScrollPane> filteredLists = new HashMap<>();
+    private JPanel filteredLogPanel = new JPanel();
+    private JScrollPane currentList = null;
     //TODO: selected filter paneli ekle globale, select filter fonksiyonu ekle
     // parametre olarak msg class alsın, filtreler hashmapi ekle ve gelen parametreye göre
     // gösterilen filtred paneli güncelle
@@ -42,26 +43,7 @@ public class LogPanel extends VCSPanel {
         logArea.setCaretColor(UIColorManager.DARK_PANEL_COLOR);
         logArea.setForeground(Color.WHITE);
 
-        messageModel = new DefaultListModel<>();
-        messageList = new JList<>(messageModel);
-        messageList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        messageList.setCellRenderer(new DefaultListCellRenderer(){
-            @Override
-            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-                JLabel lbl = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                if (value instanceof Message m){
-                    ArrayList<String> mes = m.getMsg2();
-                    String s1 = mes.get(0);
-                    String s2 = mes.get(1);
-                    Color c = m.getColor();
-                    String hex = String.format("#%02x%02x%02x", c.getRed(), c.getGreen(), c.getBlue());
-                    String html = String.format("<html><span style='color:white;'> %s </span> <span style='color:%s;'> %s </span></html>", s1, hex, s2);
-                    lbl.setText(html);
 
-
-                } return lbl;
-            }
-        });
 
 
         TitledBorder logTitledBorder = new TitledBorder("Log: ");
@@ -72,47 +54,75 @@ public class LogPanel extends VCSPanel {
         debugTitledBorder.setTitleColor(Color.WHITE);
         debugTitledBorder.setBorder(BorderFactory.createLineBorder(UIColorManager.DARK_MAP_BG_BLUE_COLOR,2));
 
-        //messageList.setBorder(logTitledBorder);
-        messageList.setBackground(UIColorManager.DARK_PANEL_COLOR);
-        messageList.setForeground(Color.WHITE);
-        messageList.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.getClickCount() == 2){
-                    int index = messageList.locationToIndex(e.getPoint());
-                    if (index != -1){
-                        Rectangle msgBound = messageList.getCellBounds(index, index);
-                        if (msgBound != null && msgBound.contains(e.getPoint())){
-                            Message message = messageList.getModel().getElementAt(index);
-                            showMsgDetails(message);
+        logArea.setBorder(debugTitledBorder);
+        logArea.setBackground(UIColorManager.DARK_PANEL_COLOR);
+        int width = this.getWidth();
+
+        {
+            messageModelAll = new DefaultListModel<>();
+            messageList = new JList<>(messageModelAll);
+            messageList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            messageList.setCellRenderer(new DefaultListCellRenderer() {
+                @Override
+                public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                    JLabel lbl = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                    if (value instanceof Message m) {
+                        ArrayList<String> mes = m.getMsg2();
+                        String s1 = mes.get(0);
+                        String s2 = mes.get(1);
+                        Color c = m.getColor();
+                        String hex = String.format("#%02x%02x%02x", c.getRed(), c.getGreen(), c.getBlue());
+                        String html = String.format("<html><span style='color:white;'> %s </span> <span style='color:%s;'> %s </span></html>", s1, hex, s2);
+                        lbl.setText(html);
+
+
+                    }
+                    return lbl;
+                }
+            });
+            messageList.setBackground(UIColorManager.DARK_PANEL_COLOR);
+            messageList.setForeground(Color.WHITE);
+            messageList.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getClickCount() == 2) {
+                        int index = messageList.locationToIndex(e.getPoint());
+                        if (index != -1) {
+                            Rectangle msgBound = messageList.getCellBounds(index, index);
+                            if (msgBound != null && msgBound.contains(e.getPoint())) {
+                                Message message = messageList.getModel().getElementAt(index);
+                                showMsgDetails(message);
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
 
-        logArea.setBorder(debugTitledBorder);
-        logArea.setBackground(UIColorManager.DARK_PANEL_COLOR);
-        JScrollPane scrollList = new JScrollPane(messageList);
-        scrollList.setBackground(UIColorManager.DARK_PANEL_COLOR);
-        scrollList.getViewport().getView().setBackground(UIColorManager.DARK_PANEL_COLOR);
-        scrollList.setBorder(null);
+            JScrollPane scrollListAll = new JScrollPane(messageList);
+            scrollListAll.setBackground(UIColorManager.DARK_PANEL_COLOR);
+            scrollListAll.getViewport().getView().setBackground(UIColorManager.DARK_PANEL_COLOR);
+            scrollListAll.setBorder(null);
+            scrollListAll.setPreferredSize(new Dimension(width / 2, getHeight()));
+            filteredLists.put(Message.MessageClass.ALL, scrollListAll);
+            currentList = scrollListAll;
+        }
+
         JScrollPane debugPanel = new JScrollPane(logArea);
         debugPanel.setBackground(UIColorManager.DARK_PANEL_COLOR);
         debugPanel.getViewport().getView().setBackground(UIColorManager.DARK_PANEL_COLOR);
         debugPanel.setBorder(null);
         //this.add(label);
-        int width = this.getWidth();
+
         debugPanel.setPreferredSize(new Dimension(width/2, getHeight()));
-        scrollList.setPreferredSize(new Dimension(width/2, getHeight()));
+
 
         JPanel messagePanel = new JPanel();
         messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.X_AXIS));
        // messagePanel.add(scrollList);
 
-        JPanel filterPanel = new JPanel();
-        filterPanel.setBackground(UIColorManager.DARK_PANEL_COLOR);
-        filterPanel.setLayout(new GridLayout(Message.MessageClass.values().length, 1));
+        JPanel filterButtonsPanel = new JPanel();
+        filterButtonsPanel.setBackground(UIColorManager.DARK_PANEL_COLOR);
+        filterButtonsPanel.setLayout(new GridLayout(Message.MessageClass.values().length, 1));
 
         for(Message.MessageClass mc : Message.MessageClass.values()){
             JButton button = new JButton(mc.getCode());
@@ -126,18 +136,34 @@ public class LogPanel extends VCSPanel {
                     selectedButtonVisual(button);
                 }
             });
-            filterPanel.add(button);
+            filterButtonsPanel.add(button);
         }
         //messagePanel.add(filterPanel);
         this.add(messagePanel);
         this.add(debugPanel);
 
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollList, filterPanel);
-        splitPane.setResizeWeight(0.9);
+        filteredLogPanel.setLayout(new BoxLayout(filteredLogPanel, BoxLayout.X_AXIS));
+        filteredLogPanel.setBackground(UIColorManager.DARK_PANEL_COLOR);
+
+
+        selectFilter(Message.MessageClass.ALL);
+        //filteredLogPanel.add(currentList);
+
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, filteredLogPanel, filterButtonsPanel);
+        splitPane.setResizeWeight(1);
         splitPane.setDividerSize(0);
         splitPane.setBorder(logTitledBorder);
         splitPane.setBackground(UIColorManager.DARK_PANEL_COLOR);
         messagePanel.add(splitPane, BorderLayout.CENTER);
+    }
+
+    private void selectFilter(Message.MessageClass messageClass) {
+        if(currentList != null){
+            filteredLogPanel.remove(currentList);
+            currentList = filteredLists.get(messageClass);
+            filteredLogPanel.add(currentList);
+        }
     }
 
     private void selectedButtonVisual(JButton button){
@@ -155,7 +181,7 @@ public class LogPanel extends VCSPanel {
     public void toLog(Message message){
         if(message.getSrcID().equals("HQ") || message.getSrcID().charAt(0) == 'A')
             if (message.getTargetID() != null && (message.getTargetID().equals("HQ") || message.getTargetID().charAt(0) == 'A')){
-                messageModel.insertElementAt(message, 0);
+                messageModelAll.insertElementAt(message, 0);
             }
         //messageList.ensureIndexIsVisible(messageModel.size() - 1);
     }
@@ -189,7 +215,7 @@ public class LogPanel extends VCSPanel {
      public void clearLogArea(){
         try {
             logArea.getStyledDocument().remove(0, logArea.getStyledDocument().getLength());
-            messageModel.removeAllElements();
+            messageModelAll.removeAllElements();
             messageList.repaint();
         } catch (BadLocationException e){
             e.printStackTrace();
