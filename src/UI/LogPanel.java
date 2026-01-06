f19afcf20f4ba934db16f84beba3956cd8ad2edb
sedakcgl <200404326+sedakcgl@users.noreply.github.com>
2026-01-06 16:36:31 +0300

    Limited the Logs to 200

--- src/UI/LogPanel.java
+++ src/UI/LogPanel.java
@@ -196,10 +196,14 @@ public class LogPanel extends VCSPanel {
                 ((DefaultListModel<Message>) ((JList<Message>) filteredListScrollPanes.
                         get(message.type.getMessageClass()).getViewport().getView()).getModel()).
                         insertElementAt(message, 0); // Kendi filtered listesine ekler
-//
-//                if(((JList<Message>) filteredListScrollPanes.get(Message.MessageClass.ALL).getViewport().getView()).getModel().getSize() == 100){
-//                    ((DefaultListModel<Message>) ((JList<Message>) filteredListScrollPanes.get(Message.MessageClass.ALL).getViewport().getView()).getModel()).remove(99);
-//                } //TODO lag ı azaltmak için yazmıştım daha iyisi olur mu bakalım olmazsa bunu kullanalım
+
+                if(((JList<Message>) filteredListScrollPanes.get(Message.MessageClass.ALL).getViewport().getView()).getModel().getSize() == 200){
+                    ((DefaultListModel<Message>) ((JList<Message>) filteredListScrollPanes.get(Message.MessageClass.ALL).getViewport().getView()).getModel()).remove(199);
+                }
+                if (((JList<Message>) filteredListScrollPanes.get(message.type.getMessageClass()).getViewport().getView()).getModel().getSize() == 200){
+                    ((DefaultListModel<Message>) ((JList<Message>) filteredListScrollPanes.get(message.type.getMessageClass()).getViewport().getView()).getModel()).remove(199);
+                }
+                //TODO lag ı azaltmak için yazmıştım daha iyisi olur mu bakalım olmazsa bunu kullanalım
             }
         //messageList.ensureIndexIsVisible(messageModel.size() - 1);
     }
